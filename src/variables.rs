cce21560afc4312721ab4d2a366793f4394b9680
Triatan <triatanv@gmail.com>
2025-05-14 21:10:07 +0200

    update

---
+++ src/variables.rs
@@ -0,0 +1,22 @@
+fn variables() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    
+    another_function(5);
+}
+
+fn another_function(x: i32) {
+    /*hello world*/
+    println!("Another function. x is: {}", x);
+    if x < 5 {
+        println!("The value of x is: {}", x);
+    }
+
+    if x % 4 == 0 {}
+    else if x % 4 == 1 {  }
+    else if x % 4 == 2 {}
+    else { println!("The value of x is: {}", x); }
+}
+
